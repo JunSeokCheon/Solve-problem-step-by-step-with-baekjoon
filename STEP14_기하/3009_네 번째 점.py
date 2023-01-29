fbfdba5facdc42f92f196bcda9fd5d3e8e5582f8
JunSeokCheon <goosw775@naver.com>
2023-01-29 13:48:38 +0900

    update

---
+++ STEP14_기하/3009_네 번째 점.py
@@ -0,0 +1,17 @@
+import sys
+
+x_list = []
+y_list = []
+
+for _ in range(3):
+    x, y = map(int, sys.stdin.readline().split())
+    x_list.append(x)
+    y_list.append(y)
+
+for x in x_list:
+    if x_list.count(x) == 1:
+        print(x, end=' ')
+
+for y in y_list:
+    if y_list.count(y) == 1:
+        print(y, end=' ')    
